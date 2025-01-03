9da1806b6de8b456b1cf19fc44a129eb7a1163de
confusedDip <anihero4@gmail.com>
2025-01-03 01:11:45 -0700

    withdraw functionality

--- FundMe.sol
+++ FundMe.sol
@@ -7,12 +7,34 @@ contract FundMe {
     using PriceConverter for uint256;   // Attaching the library functions for uint256 variables
 
     uint256 internal minimumUSD = 5e18; // Sets the minimum funding value
-    mapping (address funder => uint256 amount) funders; // Keeps a track of the funders
+    address[] funders; // Keeps a track of the funders
+    mapping (address funder => uint256 amount) fundedAmounts; // Keeps a track of the funded amounts
 
+    address public owner; // Deployer of the contract
+
+    constructor() {
+        owner = msg.sender; // The deployer of the smart contract is the owner of the contract
+    }
+
+    // A method that allows funders to send money through the function
     function fund() public payable{
-        // require(getConversionRate(msg.value) >= minimumUSD, "Didn't send enough ether. Minimum USD 5.00");
         require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough ether. Minimum USD 5.00");
-        funders[msg.sender] += msg.value.getConversionRate();    
+        fundedAmounts[msg.sender] += msg.value.getConversionRate();    
+    }
+
+    function withdraw() public {
+
+        require(msg.sender == owner, "Can only be withdrawn by the Owner!");
+        for (uint funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            fundedAmounts[funder] = 0;  // Sets the amount funded to be Zero
+        }
+        // Reset the funders array
+        funders = new address[](0);
+
+        // Withdraw the balance
+        (bool callSuccess,) = payable(msg.sender).call{value:address(this).balance}("");
+        require(callSuccess, "Transaction Unsuccessful");
     }
 
     
